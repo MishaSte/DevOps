4eb1d312f2931112e6653e6153e0ec43f131fbc1
MishaSte <mishastepanitsa@gmail.com>
2025-01-10 21:29:26 +0000

    HotFix: the implementation of FuncA returning its value as the sum of the first 3 elements of an infinite sequence

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,7 +1,28 @@
 #include "FuncA.h"
 #include <cmath>
+#include <iostream>
 
-double FuncA::calculate(double x) {
-return sin(x);
+unsigned long long factorial(int num) {
+unsigned long long result = 1;
+for (int i = 1; i <= num; ++i) {
+result *= i;
+}
+return result;
+}
+
+double FuncA::calculate(double x, int n) {
+double sum = 0.0;
+for (int i = 0; i < n; ++i) {
+// Чисельник: (-1)^n * (2n)!
+long long numerator = pow(-1, i) * factorial(2 * i);
+// Знаменник: (1 - 2n) * (n!)^2 * 4^n
+long long denominator = (1 - 2 * i) * pow(factorial(i), 2) * pow(4, i);
+// Обчислюємо член ряду
+double term = static_cast<double>(numerator) / denominator * pow(x, i);
+// Додаємо член до суми
+sum += term;
+}
+
+return sum;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "FuncA.h"
+#include <iomanip>
 
 int main() {
-FuncA func;
-double result = func.calculate(0);
-std::cout << "Result: " << result << std::endl;
+    FuncA func;
+
+int n = 3;
+double x = 0.5;
+double r = func.calculate (x, n);
+std::cout << "For x = " << x << " and n = " << n << ", the result is: " << r << std::endl;
+
 return 0;
 }
